70315c52900ed1436dfcf1972410d3dc7a0dccd3
vincent-max-wilkes <vincent.wilkes@web.de>
2026-07-15 16:02:14 +0200

    Update 2015-3.2.c

--- 2015-3.2.c
+++ 2015-3.2.c
@@ -22,37 +22,37 @@ int main(void)
     position robot = {.x = 0, .y = 0};
 
     while(fscanf(ptr, "%c", &direction) != EOF)	{
-	if(count % 2 == 0){
-	    gifter = &santa;
+		if(count % 2 == 0){
+		    gifter = &santa;
+		}
+		else{
+		    gifter = &robot;
+		}
+		    if(direction == '^'){
+				gifter->y--;
+				coordinate[row+(gifter->y)][column+(gifter->x)] += 1; 
+		    }
+		    else if(direction == 'v'){
+				gifter->y++;
+				coordinate[row+(gifter->y)][column+(gifter->x)] += 1; 
+		    }
+		    else if(direction == '>'){
+				gifter->x++;
+				coordinate[row+(gifter->y)][column+(gifter->x)] += 1; 
+		    }
+		    else if(direction == '<'){
+				gifter->x--;
+				coordinate[row+(gifter->y)][column+(gifter->x)] += 1; 
+		    }
+	
+		count++;
 	}
-	else{
-	    gifter = &robot;
-	}
-	    if(direction == '^'){
-		gifter->y--;
-		coordinate[row+(gifter->y)][column+(gifter->x)] += 1; 
-	    }
-	    else if(direction == 'v'){
-		gifter->y++;
-		coordinate[row+(gifter->y)][column+(gifter->x)] += 1; 
-	    }
-	    else if(direction == '>'){
-		gifter->x++;
-		coordinate[row+(gifter->y)][column+(gifter->x)] += 1; 
-	    }
-	    else if(direction == '<'){
-		gifter->x--;
-		coordinate[row+(gifter->y)][column+(gifter->x)] += 1; 
-	    }
-
-	count++;
-    }
 
     for(row = 0; row < 1000; row++){
-	for(column = 0; column < 1000; column++){
-	    if(coordinate[row][column] != 0)
-		houses++;
-	}
+		for(column = 0; column < 1000; column++){
+		    if(coordinate[row][column] != 0)
+				houses++;
+		}
     }
 
     printf("Santa and his Robot visit %d houses\n", houses);
